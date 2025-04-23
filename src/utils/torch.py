cb5222488763dc2612534f1409df1672c466bb4b
tetaud-sebastien <tetaud.sebastien@gmail.com>
2025-04-23 11:43:34 +0000

    Code Organization and Structure

--- src/utils/torch.py
+++ src/utils/torch.py
@@ -13,13 +13,21 @@ def seed_everything(seed):
     Args:
         seed (int): Number of the seed.
     """
-    random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+
+    try:
+        random.seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        logger.info("seed everything successful")
+
+    except Exception as e:
+        logger.error(f"Failed to seed everything: {e}")
+        raise
+
 
 
 def count_parameters(model, all=False):
